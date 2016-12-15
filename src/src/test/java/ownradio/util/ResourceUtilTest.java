7c72d6aae3b67d69a7a804fab11576fb57d89c47
Tanat <researcher2286@gmail.com>
2016-12-15 11:39:33 +0600

    Причесать код

--- src/src/test/java/ownradio/util/ResourceUtilTest.java
+++ src/src/test/java/ownradio/util/ResourceUtilTest.java
@@ -1,6 +1,11 @@
 package ownradio.util;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 
@@ -9,14 +14,16 @@ import static org.junit.Assert.*;
 /**
  * Created by Tanat on 015 15.12.16.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ResourceUtilTest {
+
+	@Value("classpath:/media/test.mp3")
+	private Resource resource;
 
 	@Test
 	public void readMetaDataFromMediaFile() throws Exception {
-		ClassLoader classLoader = getClass().getClassLoader();
-		File file = new File(classLoader.getResource("media/test.mp3").getFile());
-
-		ResourceUtil.readMetaDataFromMediaFile(file);
+		ResourceUtil.readMetaDataFromMediaFile(resource.getFile());
 	}
 
 }
